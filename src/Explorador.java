c04074635ba471af8bc5f5162bd230970b440555
nidaeasrih368 <113137612+nidaeasrih368@users.noreply.github.com>
2024-01-21 14:01:57 +0100

    Avanzo en la clase Explorador.

--- src/Explorador.java
+++ src/Explorador.java
@@ -2,8 +2,19 @@ public class Explorador {
     private String nombre;
     private Posicion posicionActual;
 
+    // Constantes de movimiento:
+    public static final int ARRIBA = 1;
+    public static final int ABAJO = 2;
+    public static final int IZQUIERDA = 3;
+    public static final int DERECHA = 4;
+
+    /**
+     * Constructor de Explorador
+     * @param nombre , y posicionActual en la primera columna y la fila obtener aleatoriamente.
+     */
     public Explorador(String nombre){
         this.nombre = nombre;
+
         int numFilaAleatorio = (int) (Math.random()*10+1);
         this.posicionActual = new Posicion(numFilaAleatorio,0);
 
@@ -20,6 +31,28 @@ public class Explorador {
         this.posicionActual = posicionActual;
     }
 
+    /**
+     * Se modificará la posicionActual del explorador
+     * @param direccion numero entero entre 1 y 4 (constantes)
+     */
+    public void moverse(int direccion){
+        switch (direccion){
+            case ARRIBA:
+                posicionActual.setCoordenadaFila(posicionActual.getCoordenadaFila() -1);
+                break;
+            case ABAJO:
+                posicionActual.setCoordenadaFila(posicionActual.getCoordenadaFila() +1);
+                break;
+            case IZQUIERDA:
+                posicionActual.setCoordenadaCol(posicionActual.getCoordenadaCol() -1);
+                break;
+            case DERECHA:
+                posicionActual.setCoordenadaCol(posicionActual.getCoordenadaCol() +1);
+                break;
+            default:
+                System.out.println("La Direccion no es valida!");
+        }
+    }
     public int explorar(Mapa mapa){
 
         return 0;
@@ -28,9 +61,5 @@ public class Explorador {
     public int checkPosicion(Mapa mapa){
 
         return 0;
-    }
-
-    public void moverse(int direccion){
-
     }
 }
